eeb7b897a7fa77e0e1ba91853b71ef2e8ef28718
belén <56318080+blnblnbln@users.noreply.github.com>
2019-10-15 16:59:37 -0300

    Add files via upload

---
+++ 12.3.py
@@ -0,0 +1,15 @@
+n = -1
+while n < 0:
+    n = int(input('Ingrese un valor: '))
+suma2 = 0
+for i in range(2,n+1):
+    suma = 0
+    for a in range(2,i):
+        if i%a == 0:
+            suma += 1
+    if suma == 0:
+        suma2 += 1
+        print('el número', i, 'es primo')
+        
+print(suma2)
+            
