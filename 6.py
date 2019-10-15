58b2b530aa6dfa1529853df8746b4a9ae569f21c
belén <56318080+blnblnbln@users.noreply.github.com>
2019-10-15 17:19:55 -0300

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,9 @@
+n = int(input('Ingrese un valor: '))
+dividendo = n
+cuociente = 0
+resto = 0
+while dividendo != 0:
+    cuociente = dividendo//2
+    resto = dividendo%2
+    dividendo = cuociente
+    print(resto)
