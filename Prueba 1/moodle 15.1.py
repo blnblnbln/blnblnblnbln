4a13eb0677572b1117d59bd8752564579a795fcd
belén <56318080+blnblnbln@users.noreply.github.com>
2019-10-27 22:27:37 -0300

    Prueba 1
    Adjunto ejercicios preparación prueba 1

---
+++ Prueba 1/moodle 15.1.py
@@ -0,0 +1,14 @@
+'''
+
+Realice un programa que lea por teclado un texto, e indique:
+Cuántos dígitos posee en total. Por ejemplo, el texto "hola 45, mundo 56" posee 4 dígitos en total.
+
+'''
+
+texto = input('Ingrese un texto: ')
+suma = 0
+for c in texto:
+    if c == '1' or c == '2' or c == '3' or c == '4' or c == '5' or c == '6' or c == '7' or c == '8' or c == '9' or c == '0':
+        suma += 1
+        
+print ('El texto posee', suma, 'dígitos')
