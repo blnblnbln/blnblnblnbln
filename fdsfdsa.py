b6ee6a85679bc1de973a4df858615a169d0d6f99
belén <56318080+blnblnbln@users.noreply.github.com>
2020-03-03 16:44:16 -0300

    Add files via upload

---
+++ fdsfdsa.py
@@ -0,0 +1,16 @@
+matriz = []
+M = int(input('Ingrese la cantidad de filas: '))
+N = int(input('Ingrese la cantidad de columnas: '))
+
+for i in range (M):
+    matriz.append([])
+    for j in range (N):
+        valor = int(input('Ingrese un valor: '))
+        matriz[i].append(valor)
+print(matriz)
+
+for i in range (M):
+    for j in range (N):
+        valor = matriz [i][j]
+        print(valor, end=' ')
+    print('')
